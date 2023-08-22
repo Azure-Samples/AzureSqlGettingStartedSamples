833a7bef851d1fc59a8eca5cd55ff8624e60fd46
dlevy-msft <77136938+dlevy-msft@users.noreply.github.com>
2023-08-21 21:26:10 -0500

    Update connect.go
    Update to use Microsoft driver

--- go/connect.go
+++ go/connect.go
@@ -1,11 +1,12 @@
 package main
 
-import ( 
-    _ "github.com/denisenkom/go-mssqldb"
-    "database/sql"
-    "context"
-    "log"
-    "fmt"
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/microsoft/go-mssqldb"
 )
 
 // Replace with your own connection parameters
@@ -17,43 +18,48 @@ var database = "your_database"
 var db *sql.DB
 
 func main() {
-    var err error
+	var err error
 
-    // Create connection string
-    connString := fmt.Sprintf("server=%s;user id=%s;password=%s;database=%s",
-        server, user, password, database)
+	// Create connection string
+	connString := fmt.Sprintf("server=%s;user id=%s;password=%s;database=%s",
+		server, user, password, database)
 
-    // Create connection pool
-    db, err = sql.Open("sqlserver", connString)
-    if err != nil {
-        log.Fatal("Error creating connection pool: " + err.Error())
-    }
-    log.Printf("Connected!\n")
+	// Create connection pool
+	db, err = sql.Open("sqlserver", connString)
+	if err != nil {
+		log.Fatal("Error creating connection pool: " + err.Error())
+	}
+	log.Printf("Connected!\n")
 
-    // Close the database connection pool after program executes
-    defer db.Close()
+	// Close the database connection pool after program executes
+	defer db.Close()
 
-    SelectVersion()
+	SelectVersion()
 }
 
 // Gets and prints SQL Server version
-func SelectVersion(){
-    // Use background context
-    ctx := context.Background()
+func SelectVersion() {
+	// Use background context
+	ctx := context.Background()
 
-    // Ping database to see if it's still alive.
-    // Important for handling network issues and long queries.
-    err := db.PingContext(ctx)
-    if err != nil {
-        log.Fatal("Error pinging database: " + err.Error())
-    }
+	// Ping database to see if it's still alive.
+	// Important for handling network issues and long queries.
+	err := db.PingContext(ctx)
+	if err != nil {
+		log.Fatal("Error pinging database: " + err.Error())
+	}
 
-    var result string
+	stmt, err := db.Prepare("select @@version")
+	if err != nil {
+		log.Fatal("Prepare failed:", err.Error())
+	}
+	defer stmt.Close()
 
-    // Run query and scan for result
-    err = db.QueryRowContext(ctx, "SELECT @@version").coScan(&result)
-    if err != nil {
-        log.Fatal("Scan failed:", err.Error())
-    }
-    fmt.Printf("%s\n", result)
+	row := stmt.QueryRow()
+	var result string
+	err = row.Scan(&result)
+	if err != nil {
+		log.Fatal("Scan failed:", err.Error())
+	}
+	fmt.Printf("%s\n", result)
 }
